3abdd1c4756e4a55af6ae7b566499ccd32e205ea
huwred <huw@forum.snitz.com>
2024-03-25 10:27:02 +0000

    added new taghelper for script renderring

---
+++ MVCForum.Service/TagHelpers/RenderStoredContentTagHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SnitzCore.Service.TagHelpers
+{
+    [HtmlTargetElement("renderstoredcontent")]
+    public class RenderStoredContentTagHelper : TagHelper
+    {
+        private const string KeyAttributeName = "asp-key";
+        private const string _storageKey = "storecontent";
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        [HtmlAttributeName(KeyAttributeName)]
+        public string Key { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = String.Empty;
+
+            var storageProvider = ViewContext.TempData;
+            Dictionary<string, List<HtmlString>> storage;
+
+            if (!storageProvider.ContainsKey(_storageKey) || !(storageProvider[_storageKey] is Dictionary<string, List<HtmlString>>))
+            {
+                return;
+            }
+
+            storage = storageProvider[_storageKey] as Dictionary<string, List<HtmlString>>;
+            string html = "";
+
+            if (String.IsNullOrEmpty(Key))
+            {
+                html = String.Join("", storage.Values.SelectMany(x => x).ToList());
+            }
+            else
+            {
+                if (!storage.ContainsKey(Key)) return;
+                html = String.Join("", storage[Key]);
+            }
+
+            TagBuilder tagBuilder = new TagBuilder("dummy");
+            tagBuilder.InnerHtml.AppendHtml(html) ;
+            output.Content.AppendHtml(html);
+        }
+    } 
+}
---
+++ MVCForum.Service/TagHelpers/StoreContentTagHelper.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SnitzCore.Service.TagHelpers;
+
+    [HtmlTargetElement("storecontent")]
+    public class StoreContentTagHelper : TagHelper
+    {
+        private const string KeyAttributeName = "asp-key";
+        private const string _storageKey = "storecontent";
+        private const string _defaultListKey = "DefaultKey";
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        [HtmlAttributeName(KeyAttributeName)]
+        public string Key { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.SuppressOutput();
+            TagHelperContent childContent = await output.GetChildContentAsync();
+
+            var storageProvider = ViewContext.TempData;
+            Dictionary<string, List<HtmlString>> storage;
+            List<HtmlString> defaultList;
+
+            if (!storageProvider.ContainsKey(_storageKey) || !(storageProvider[_storageKey] is Dictionary<string,List<HtmlString>>))
+            {
+                storage = new Dictionary<string, List<HtmlString>>();
+                storageProvider[_storageKey] = storage;
+                defaultList = new List<HtmlString>();
+                storage.Add(_defaultListKey, defaultList);
+            }
+            else
+            {
+                storage = ViewContext.TempData[_storageKey] as Dictionary<string, List<HtmlString>>;
+                if (storage.ContainsKey(_defaultListKey))
+                {
+                    defaultList = storage[_defaultListKey];
+
+                }
+                else
+                {
+                    defaultList = new List<HtmlString>();
+                    storage.Add(_defaultListKey, defaultList);
+                }
+            }
+
+            if (string.IsNullOrEmpty(Key))
+            {
+                defaultList.Add(new HtmlString(childContent.GetContent()));
+            }
+            else
+            {
+                if(storage.ContainsKey(Key))
+                {
+                    storage[Key].Add(new HtmlString(childContent.GetContent()));
+                }
+                else
+                {
+                    storage.Add(Key, new List<HtmlString>() { new HtmlString(childContent.GetContent()) });
+                }
+            }
+        }
+    } 
