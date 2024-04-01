4e76fa840e0b591ac34a068f3ad56bd81c03f460
huwred <huw@forum.snitz.com>
2024-04-01 14:10:02 +0100

    Tag helper fix

--- MVC Forum/TagHelpers/TopicImageTagHelper.cs
+++ MVC Forum/TagHelpers/TopicImageTagHelper.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 using SnitzCore.Data.Extensions;
 using SnitzCore.Data.Interfaces;
 using System;
+using System.Text;
+using System.Text;
 using System.Text.Encodings.Web;
 
 namespace MVCForum.TagHelpers
@@ -38,45 +40,9 @@ namespace MVCForum.TagHelpers
             base.Process(context, output);
             output.TagMode = TagMode.StartTagAndEndTag;
             output.TagName = "i";
-
-            output.AddClass("fa",HtmlEncoder.Default);
-            if (Sticky && _config.GetIntValue("STRSTICKYTOPIC") == 1)
-            {
-                output.Attributes.Add("title","Sticky Topic");
-                output.AddClass("fa-thumb-tack",HtmlEncoder.Default);
-                output.AddClass("center",HtmlEncoder.Default);
-                return;
-            }
-            if (Status == "0")
-            {
-                output.Attributes.Add("title","Locked");
-                output.AddClass("fa-lock",HtmlEncoder.Default);
-            }
-            else if (Replies == 0)
-            {
-                output.Attributes.Add("title","No replies");
-                output.AddClass("fa-frown-o",HtmlEncoder.Default);
-            }
-            else if (Replies > 100)
-            {
-                output.Attributes.Add("title","Super charged Topic");
-                output.AddClass("fa-rocket",HtmlEncoder.Default);
-            }
-            else if (Replies > _config.GetIntValue("INTHOTTOPICNUM",25))
-            {
-                output.Attributes.Add("title","Hot Topic");
-                output.AddClass("fa-fire",HtmlEncoder.Default);
-            }
-            else if (Answered)
-            {
-                output.Attributes.Add("title","Topic answered");
-                output.AddClass("fa-folder",HtmlEncoder.Default);
-            }
-            else 
-            {
-                output.Attributes.Add("title","No new posts");
-                output.AddClass("fa-folder-o",HtmlEncoder.Default);
-            }
+            string locked = Status == "0" ? ", Locked" : "";
+            string newposts = "";
+            bool newclass = false;
 
             if (LastPost.HasValue)
             {
@@ -85,19 +51,48 @@ namespace MVCForum.TagHelpers
                 {
                     if (LastPost > lasthere)
                     {
-                        if (Status == "0")
-                        {
-                            output.Attributes.Add("title","Locked, Contains new posts");
-                            output.AddClass("fa-lock",HtmlEncoder.Default);
-                        }
-                        else
-                        {
-                            output.AddClass("new-posts",HtmlEncoder.Default);
-                            output.Attributes.Add("title","Contains new posts");
-                        }
+                        newposts = ", Contains new posts";
+                        newclass = true;
                     }
                 }
             }
+
+            output.AddClass("fa",HtmlEncoder.Default);
+            if (Sticky && _config.GetIntValue("STRSTICKYTOPIC") == 1)
+            {
+                output.Attributes.Add("title",$"Sticky Topic{locked}{newposts}");
+                output.AddClass("fa-thumb-tack",HtmlEncoder.Default);
+                output.AddClass("center",HtmlEncoder.Default);
+                if(newclass){output.AddClass("newposts",HtmlEncoder.Default);}
+                return;
+            }
+            if (Replies == 0)
+            {
+                output.Attributes.Add("title",$"No replies{locked}{newposts}");
+                output.AddClass("fa-frown-o",HtmlEncoder.Default);
+            }
+            else if (Replies > 100)
+            {
+                output.Attributes.Add("title",$"Super charged Topic{locked}{newposts}");
+                output.AddClass("fa-rocket",HtmlEncoder.Default);
+            }
+            else if (Replies > _config.GetIntValue("INTHOTTOPICNUM",25))
+            {
+                output.Attributes.Add("title",$"Hot Topic{locked}{newposts}");
+                output.AddClass("fa-fire",HtmlEncoder.Default);
+            }
+            else if (Answered)
+            {
+                output.Attributes.Add("title",$"Topic answered{locked}{newposts}");
+                output.AddClass("fa-folder",HtmlEncoder.Default);
+            }
+            else 
+            {
+                output.Attributes.Add("title",$"No new posts{locked}{newposts}");
+                output.AddClass("fa-folder-o",HtmlEncoder.Default);
+            }
+            if(newclass){output.AddClass("newposts",HtmlEncoder.Default);}
+
             output.AddClass("center",HtmlEncoder.Default);
         }
     }
