0539c796d94e56a970dfbba555ac10559e15dffd
huwred <huw@forum.snitz.com>
2025-06-12 13:20:03 +0200

    disable EF FirstWithoutOrderBy warning messages

--- MVC Forum/Program.cs
+++ MVC Forum/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using MVCForum.Extensions;
 using SnitzCore.Data;
 using SnitzCore.Data.Extensions;
@@ -15,7 +14,6 @@ using SnitzCore.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Reflection;
 using System.Threading.Tasks;
 using Hangfire;
@@ -50,7 +48,11 @@ builder.Services.Configure<SnitzForums>(builder.Configuration.GetSection("SnitzF
 builder.Services.Configure<EmailConfiguration>(builder.Configuration.GetSection("MailSettings"));
 builder.Services.AddDbContext<SnitzDbContext>(options =>
 {
-    options.ConfigureWarnings(w => w.Ignore(SqlServerEventId.SavepointsDisabledBecauseOfMARS));
+
+    options.ConfigureWarnings(warnings => warnings
+        .Ignore(CoreEventId.FirstWithoutOrderByAndFilterWarning)
+        .Ignore(SqlServerEventId.SavepointsDisabledBecauseOfMARS)
+    );
     options.UseDatabase(builder.Configuration.GetConnectionString("DBProvider"), builder.Configuration, System.IO.Path.Combine(builder.Environment.ContentRootPath, "App_Data"));
     options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
     options.EnableDetailedErrors();
@@ -219,43 +221,6 @@ builder.Services.AddProblemDetails();
 var app = builder.Build();
 app.MigrateDatabase();
 
-//app.Use(async (ctx, next) =>
-//{
-//    await next();
-
-//    if (ctx.Response.StatusCode == 404 && !ctx.Response.HasStarted)
-//    {
-//        //Re-execute the request so the user gets the error page
-//        string originalPath = ctx.Request.Path.Value;
-//        ctx.Items["originalPath"] = originalPath;
-//        ctx.Request.Path = "/error/404";
-//        await next();
-//    }
-//});
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseDeveloperExceptionPage();
-//    app.UseMigrationsEndPoint();
-
-//}
-//else
-//{
-//    app.UseExceptionHandler("/error/handle-exception");
-//    app.UseStatusCodePages(context =>
-//    {
-//        var response = context.HttpContext.Response;
-//        if (response.StatusCode == (int)HttpStatusCode.Unauthorized)
-//        {
-//            response.Redirect("/Account/Login");
-//        }
-//        return Task.CompletedTask;
-//    });
-//    app.UseStatusCodePagesWithReExecute("/error/{0}");
-//    //The default HSTS value is 30 days.You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
 app.UseExceptionHandler();
 app.UseHttpsRedirection();
 
