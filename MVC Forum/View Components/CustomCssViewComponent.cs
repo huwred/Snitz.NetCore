020da5d6709186c4f1dcb8fff8bf52291eca969e
huwred <huw@forum.snitz.com>
2025-07-01 11:44:16 +0200

    Default theme change fix

--- MVC Forum/View Components/CustomCssViewComponent.cs
+++ MVC Forum/View Components/CustomCssViewComponent.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SnitzCore.Data.Interfaces;
+using SnitzCore.Data.Models;
 using System.Threading.Tasks;
 
 namespace MVCForum.View_Components
@@ -7,16 +8,18 @@ namespace MVCForum.View_Components
     public class SnitzThemeCssViewComponent : ViewComponent
     {
         private readonly ISnitzCookie _cookie;
+        private readonly ISnitzConfig _config;
 
-        public SnitzThemeCssViewComponent(ISnitzCookie snitzCookie)
+        public SnitzThemeCssViewComponent(ISnitzCookie snitzCookie,ISnitzConfig snitzConfig)
         {
             _cookie = snitzCookie;
+            _config = snitzConfig;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var theme = _cookie.GetCookieValue("snitztheme");
-            return await Task.FromResult((IViewComponentResult)View("theme",theme??"SnitzTheme"));
+            return await Task.FromResult((IViewComponentResult)View("theme",theme??_config.GetValueWithDefault("STRDEFAULTTHEME", "SnitzTheme")));
         }
     }
 }
