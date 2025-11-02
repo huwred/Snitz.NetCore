4268afda99ed875e2adfcb491e176ed0de0f6d22
huwred <huw@forum.snitz.com>
2025-11-02 12:20:39 +0100

    updated localtime calculation using members timezone

--- MVCForum.Service/Extensions/DateTimeExtensions.cs
+++ MVCForum.Service/Extensions/DateTimeExtensions.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Org.BouncyCastle.Pqc.Crypto.Lms;
 using SnitzCore.Data.Interfaces;
 using SnitzCore.Data.Models;
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Net;
 
 namespace SnitzCore.Service.Extensions
@@ -22,6 +25,66 @@ namespace SnitzCore.Service.Extensions
 
             return date.LocalTime(snitzCookie).ToString(format + ", HH:mm", CultureInfo.CurrentCulture);
         }
+        /// <summary>
+        /// Converts the specified UTC <see cref="DateTime"/> to the local time of the specified user based on their
+        /// time zone settings.
+        /// </summary>
+        /// <remarks>This method retrieves the user's time zone information from their claims and caches
+        /// it for performance optimization. If the time zone cannot be determined or an error occurs during the
+        /// conversion, the method falls back to returning the input time as UTC.</remarks>
+        /// <param name="date">The UTC <see cref="DateTime"/> to be converted.</param>
+        /// <param name="manager">The <see cref="UserManager{TUser}"/> instance used to retrieve the user's claims.</param>
+        /// <param name="user">The user whose time zone settings are used for the conversion.</param>
+        /// <returns>A <see cref="DateTime"/> representing the input time converted to the user's local time. If the user's time
+        /// zone is not set or cannot be determined, the input time is returned as UTC.</returns>
+        public static DateTime LocalTime(this DateTime date,UserManager<ForumUser> manager, ForumUser user)
+        {
+            var userTimeZone = CacheProvider.GetOrCreate("TimeZone_" + user, () =>
+            {
+                var claims = manager.GetClaimsAsync(user).Result;
+                var timeZoneClaim = claims.FirstOrDefault(c => c.Type == "TimeZone");
+                if (timeZoneClaim != null)
+                {
+                    string timeZoneValue = timeZoneClaim.Value;
+                    return TimeZoneInfo.FindSystemTimeZoneById(timeZoneValue);
+                    // Use the value as needed
+                } // Get user's time zone ID from claims
+                return null;
+            }, TimeSpan.FromMinutes(10));
+
+
+            if (userTimeZone == null)
+            {
+                // Fallback to UTC if not set
+                return DateTime.SpecifyKind(date, DateTimeKind.Utc);
+            }
+            try
+            {
+                
+                //TimeZoneInfo userTimeZone = TimeZoneInfo.FindSystemTimeZoneById(userTimeZoneId);
+                DateTimeOffset userDateTimeOffset = TimeZoneInfo.ConvertTime(DateTime.SpecifyKind(date, DateTimeKind.Utc), userTimeZone);
+
+                return userDateTimeOffset.DateTime;
+            }
+            catch (Exception)
+            {
+                // Fallback to UTC if not set
+                return DateTime.SpecifyKind(date, DateTimeKind.Utc);
+            }
+
+        }
+        /// <summary>
+        /// Converts the specified UTC <see cref="DateTime"/> to the local time of the user based on their time zone
+        /// settings.
+        /// </summary>
+        /// <remarks>This method retrieves the user's time zone from a cookie via the provided <paramref
+        /// name="snitzCookie"/>. If the time zone is not set or invalid, the method falls back to returning the input
+        /// <paramref name="date"/> as UTC. The user's time zone information is cached for 10 minutes to optimize
+        /// performance.</remarks>
+        /// <param name="date">The UTC <see cref="DateTime"/> to be converted.</param>
+        /// <param name="snitzCookie">An implementation of <see cref="ISnitzCookie"/> used to retrieve the user's time zone information.</param>
+        /// <returns>A <see cref="DateTime"/> representing the user's local time if their time zone is available; otherwise, the
+        /// original <paramref name="date"/> adjusted to UTC.</returns>
         public static DateTime LocalTime(this DateTime date, ISnitzCookie snitzCookie)
         {
             var user = snitzCookie.CookieUser();
